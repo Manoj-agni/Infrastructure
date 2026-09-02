566390c3821b4319db7f687ba8b21d2363bdf412
Manoj-agni <nmanoj2251999@gmail.com>
2026-09-02 12:48:41 +0530

    Update Terraform backend configuration

--- 2-eks/backend.tf
+++ 2-eks/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "itkannadigaru-infra-statefile-backup"
-    key            = "itkannadigaru/2-eks/terraform.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "itkannadigaru-terraform-locks"
+    bucket         = "manu1999-infra-statefile-backup"
+    key            = "manu1999/2-eks/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "manu1999-terraform-locks"
     encrypt        = true
   }
 }

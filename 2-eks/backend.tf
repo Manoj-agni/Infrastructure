fdb59fcb8c28d568a46aaeda11ca82984d167900
Manoj-agni <nmanoj2251999@gmail.com>
2026-09-04 12:24:41 +0530

    Update S3 bucket and DynamoDB table for backend

--- 2-eks/backend.tf
+++ 2-eks/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "manu1999-infra-statefile-backup"
-    key            = "manu1999/2-eks/terraform.tfstate"
+    bucket         = "maruti22-infra-statefile-backup"
+    key            = "maruti22/2-eks/terraform.tfstate"
     region         = "us-west-2"
-    dynamodb_table = "manu1999-terraform-locks"
+    dynamodb_table = "maruti-terraform-locks"
     encrypt        = true
   }
 }

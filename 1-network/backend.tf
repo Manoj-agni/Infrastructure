5acc196affa30570ac22cf822192b638c2411ba2
Manoj-agni <nmanoj2251999@gmail.com>
2026-09-04 12:22:34 +0530

    Update S3 bucket and key for Terraform backend

--- 1-network/backend.tf
+++ 1-network/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "manu1999-infra-statefile-backup"
-    key            = "manu1999/1-network/terraform.tfstate"
+    bucket         = "maruti22-infra-statefile-backup"
+    key            = "maruti22/1-network/terraform.tfstate"
     region         = "us-west-2"
     dynamodb_table = "manu1999-terraform-locks"
     encrypt        = true

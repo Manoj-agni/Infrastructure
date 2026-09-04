91f851bf76bab7f39648a616bfa5dd3308f93ca3
Manoj-agni <nmanoj2251999@gmail.com>
2026-09-04 12:25:25 +0530

    Update S3 bucket and key for remote state

--- 2-eks/data.tf
+++ 2-eks/data.tf
@@ -2,8 +2,8 @@ data "terraform_remote_state" "network" {
   backend = "s3"
 
   config = {
-    bucket = "manu1999-infra-statefile-backup"
-    key    = "manu1999/1-network/terraform.tfstate"
+    bucket = "maruti22-infra-statefile-backup"
+    key    = "maruti22/1-network/terraform.tfstate"
     region = "us-west-2"
   }
 }
